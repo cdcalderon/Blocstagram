f4fa03d5b35a8d6afe4c622829deb6be8ea78390
carlos calderon <cdcalderon@gmail.com>
2015-11-03 03:26:02 -0600

    initial commit

---
+++ Blocstagram/Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Carlos Calderon on 11/3/15.
+//  Copyright (c) 2015 Carlos Calderon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
