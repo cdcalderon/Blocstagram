f27433dc83a2ed79c62a8ed4fe46a4dddddd0b09
carlos calderon <cdcalderon@gmail.com>
2015-11-22 21:59:46 -0600

    checkpoint 38 complete

--- Blocstagram/Blocstagram/DataSource.h
+++ Blocstagram/Blocstagram/DataSource.h
@@ -22,7 +22,7 @@ typedef void (^NewItemCompletionBlock)(NSError *error);
 
 - (void) requestNewItemsWithCompletionHandler:(NewItemCompletionBlock)completionHandler;
 - (void) requestOldItemsWithCompletionHandler:(NewItemCompletionBlock)completionHandler;
-
+- (void) downloadImageForMediaItem:(Media *)mediaItem;
 + (NSString *) instagramClientID;
 
 @end
