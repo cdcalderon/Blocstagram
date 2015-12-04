9ba15f9ffc387eafbc47226acafebdee7114fd11
carlos calderon <cdcalderon@gmail.com>
2015-12-04 03:15:05 -0600

    add commentView

---
+++ Blocstagram/Blocstagram/ComposeCommentView.h
@@ -0,0 +1,27 @@
+//
+//  ComposeCommentView.h
+//  
+//
+//  Created by Carlos Calderon on 12/4/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@class ComposeCommentView;
+
+@protocol ComposeCommentViewDelegate <NSObject>
+- (void) commentViewDidPressCommentButton:(ComposeCommentView *)sender;
+- (void) commentView:(ComposeCommentView *)sender textDidChange:(NSString *)text;
+- (void) commentViewWillStartEditing:(ComposeCommentView *)sender;
+
+@end
+
+@interface ComposeCommentView : UIView
+
+@property (nonatomic, weak) NSObject <ComposeCommentViewDelegate> *delegate;
+@property (nonatomic, assign) BOOL isWritingComment;
+@property (nonatomic, strong) NSString *text;
+- (void) stopComposingComment;
+
+@end
---
+++ Blocstagram/Blocstagram/ComposeCommentView.m
@@ -0,0 +1,53 @@
+//
+//  ComposeCommentView.m
+//  
+//
+//  Created by Carlos Calderon on 12/4/15.
+//
+//
+
+#import "ComposeCommentView.h"
+
+@interface ComposeCommentView () <UITextViewDelegate>
+
+@property (nonatomic, strong) UITextView *textView;
+@property (nonatomic, strong) UIButton *button;
+
+@end
+
+
+@implementation ComposeCommentView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        self.textView = [UITextView new];
+        self.textView.delegate = self;
+        
+        self.button = [UIButton buttonWithType:UIButtonTypeCustom];
+        [self.button setAttributedTitle:[self commentAttributedString] forState:UIControlStateNormal];
+        [self.button addTarget:self action:@selector(commentButtonPressed:) forControlEvents:UIControlEventTouchUpInside];
+        
+        [self addSubview:self.textView];
+        [self.textView addSubview:self.button];
+    }
+    return self;
+}
+
+- (NSAttributedString *) commentAttributedString {
+    NSString *baseString = NSLocalizedString(@"COMMENT", @"comment button text");
+    NSRange range = [baseString rangeOfString:baseString];
+    
+    NSMutableAttributedString *commentString = [[NSMutableAttributedString alloc] initWithString:baseString];
+    
+    [commentString addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"HelveticaNeue-Bold" size:10] range:range];
+    [commentString addAttribute:NSKernAttributeName value:@1.3 range:range];
+    [commentString addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:0.933 green:0.933 blue:0.933 alpha:1] range:range];
+    
+    return commentString;
+}
+
+
+@end
