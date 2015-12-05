d411e28e07f42d7f149b315d165dccef833ed170
carlos calderon <cdcalderon@gmail.com>
2015-12-05 00:49:15 -0600

    checkpoint-40-complete

--- Blocstagram/Blocstagram/ComposeCommentView.m
+++ Blocstagram/Blocstagram/ComposeCommentView.m
@@ -36,6 +36,34 @@
     return self;
 }
 
+- (void) layoutSubviews {
+    [super layoutSubviews];
+    
+    self.textView.frame = self.bounds;
+    
+    if (self.isWritingComment) {
+        self.textView.backgroundColor = [UIColor colorWithRed:0.933 green:0.933 blue:0.933 alpha:1]; /*#eeeeee*/
+        self.button.backgroundColor = [UIColor colorWithRed:0.345 green:0.318 blue:0.424 alpha:1]; /*#58516c*/
+        
+        CGFloat buttonX = CGRectGetWidth(self.bounds) - CGRectGetWidth(self.button.frame) - 20;
+        self.button.frame = CGRectMake(buttonX, 10, 80, 20);
+    } else {
+        self.textView.backgroundColor = [UIColor colorWithRed:0.898 green:0.898 blue:0.898 alpha:1]; /*#e5e5e5*/
+        self.button.backgroundColor = [UIColor colorWithRed:0.6 green:0.6 blue:0.6 alpha:1]; /*#999999*/
+        
+        self.button.frame = CGRectMake(10, 10, 80, 20);
+    }
+    
+    CGSize buttonSize = self.button.frame.size;
+    buttonSize.height += 20;
+    buttonSize.width += 20;
+    CGFloat blockX = CGRectGetWidth(self.textView.bounds) - buttonSize.width;
+    CGRect areaToBlockText = CGRectMake(blockX, 0, buttonSize.width, buttonSize.height);
+    UIBezierPath *buttonPath = [UIBezierPath bezierPathWithRect:areaToBlockText];
+    
+    self.textView.textContainer.exclusionPaths = @[buttonPath];
+}
+
 - (NSAttributedString *) commentAttributedString {
     NSString *baseString = NSLocalizedString(@"COMMENT", @"comment button text");
     NSRange range = [baseString rangeOfString:baseString];
@@ -47,6 +75,71 @@
     [commentString addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed:0.933 green:0.933 blue:0.933 alpha:1] range:range];
     
     return commentString;
+}
+
+- (void) stopComposingComment {
+    [self.textView resignFirstResponder];
+}
+
+#pragma mark - Setters & Getters
+
+- (void) setIsWritingComment:(BOOL)isWritingComment {
+    [self setIsWritingComment:isWritingComment animated:NO];
+}
+
+- (void) setIsWritingComment:(BOOL)isWritingComment animated:(BOOL)animated {
+    _isWritingComment = isWritingComment;
+    
+    if (animated) {
+        [UIView animateWithDuration:0.2 animations:^{
+            [self layoutSubviews];
+        }];
+
+        [self layoutSubviews];
+    }
+}
+
+- (void) setText:(NSString *)text {
+    _text = text;
+    self.textView.text = text;
+    self.textView.userInteractionEnabled = YES;
+   
+    self.isWritingComment = text.length > 0;
+}
+
+#pragma mark - Button Target
+
+- (void) commentButtonPressed:(UIButton *) sender {
+    if (self.isWritingComment) {
+        [self.textView resignFirstResponder];
+        self.textView.userInteractionEnabled = NO;
+        [self.delegate commentViewDidPressCommentButton:self];
+    } else {
+        [self setIsWritingComment:YES animated:YES];
+        [self.textView becomeFirstResponder];
+    }
+}
+
+#pragma mark - UITextViewDelegate
+
+- (BOOL)textViewShouldBeginEditing:(UITextView *)textView {
+    [self setIsWritingComment:YES animated:YES];
+    [self.delegate commentViewWillStartEditing:self];
+    
+    return YES;
+}
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+    NSString *newText = [textView.text stringByReplacingCharactersInRange:range withString:text];
+    [self.delegate commentView:self textDidChange:newText];
+    return YES;
+}
+
+- (BOOL)textViewShouldEndEditing:(UITextView *)textView {
+    BOOL hasComment = (textView.text.length > 0);
+    [self setIsWritingComment:hasComment animated:YES];
+    
+    return YES;
 }
 
 
--- Blocstagram/Blocstagram/MediaTableViewCell.m
+++ Blocstagram/Blocstagram/MediaTableViewCell.m
@@ -11,9 +11,9 @@
 #import "Comment.h"
 #import "User.h"
 #import "LikeButton.h"
+#import "ComposeCommentView.h"
 
-
-@interface MediaTableViewCell () <UIGestureRecognizerDelegate>
+@interface MediaTableViewCell () <UIGestureRecognizerDelegate, ComposeCommentViewDelegate>
 
 @property (nonatomic, strong) UIImageView *mediaImageView;
 @property (nonatomic, strong) UILabel *usernameAndCaptionLabel;
@@ -25,6 +25,7 @@
 @property (nonatomic, strong) UITapGestureRecognizer *tapGestureRecognizer;
 @property (nonatomic, strong) UILongPressGestureRecognizer *longPressGestureRecognizer;
 @property (nonatomic, strong) LikeButton *likeButton;
+@property (nonatomic, strong) ComposeCommentView *commentView;
 
 @end
 
@@ -68,22 +69,26 @@ static NSParagraphStyle *paragraphStyle;
         [self.likeButton addTarget:self action:@selector(likePressed:) forControlEvents:UIControlEventTouchUpInside];
         self.likeButton.backgroundColor = usernameLabelGray;
         
+        self.commentView = [[ComposeCommentView alloc] init];
+        self.commentView.delegate = self;
         
-        for (UIView *view in @[self.mediaImageView, self.usernameAndCaptionLabel, self.commentLabel, self.likeButton]) {
+        
+        for (UIView *view in @[self.mediaImageView, self.usernameAndCaptionLabel, self.commentLabel, self.likeButton,  self.commentView]) {
             [self.contentView addSubview:view];
             view.translatesAutoresizingMaskIntoConstraints = NO;
         }
         
-        NSDictionary *viewDictionary = NSDictionaryOfVariableBindings(_mediaImageView, _usernameAndCaptionLabel, _commentLabel, _likeButton);
+        NSDictionary *viewDictionary = NSDictionaryOfVariableBindings(_mediaImageView, _usernameAndCaptionLabel, _commentLabel, _likeButton, _commentView);
         
         [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_mediaImageView]|" options:kNilOptions metrics:nil views:viewDictionary]];
         [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_usernameAndCaptionLabel]|" options:kNilOptions metrics:nil views:viewDictionary]];
         [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_usernameAndCaptionLabel][_likeButton(==38)]|" options:NSLayoutFormatAlignAllTop | NSLayoutFormatAlignAllBottom metrics:nil views:viewDictionary]];
         
-        [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_mediaImageView][_usernameAndCaptionLabel][_commentLabel]"
+        [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_mediaImageView][_usernameAndCaptionLabel][_commentLabel][_commentView(==100)]"
                                                                                  options:kNilOptions
                                                                                  metrics:nil
                                                                                    views:viewDictionary]];
+        [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_commentView]|" options:kNilOptions metrics:nil views:viewDictionary]];
         
         self.imageHeightConstraint = [NSLayoutConstraint constraintWithItem:_mediaImageView
                                                                   attribute:NSLayoutAttributeHeight
@@ -198,6 +203,8 @@ static NSParagraphStyle *paragraphStyle;
     self.mediaImageView.image = _mediaItem.image;
     self.usernameAndCaptionLabel.attributedText = [self usernameAndCaptionString];
     self.commentLabel.attributedText = [self commentString];
+    self.likeButton.likeButtonState = mediaItem.likeState;
+    self.commentView.text = mediaItem.temporaryComment;
     //self.likesLabel.text = @(mediaItem.likeCount).stringValue;
 
 }
@@ -223,7 +230,7 @@ static NSParagraphStyle *paragraphStyle;
     [layoutCell layoutIfNeeded];
     
     // The height will be wherever the bottom of the comments label is
-    return CGRectGetMaxY(layoutCell.commentLabel.frame);
+    return CGRectGetMaxY(layoutCell.commentView.frame);
 }
 
 #pragma mark - Image View
@@ -248,5 +255,23 @@ static NSParagraphStyle *paragraphStyle;
 
 - (void) likePressed:(UIButton *)sender {
     [self.delegate cellDidPressLikeButton:self];
+}
+
+#pragma mark - ComposeCommentViewDelegate
+
+- (void) commentViewDidPressCommentButton:(ComposeCommentView *)sender {
+    [self.delegate cell:self didComposeComment:self.mediaItem.temporaryComment];
+}
+
+- (void) commentView:(ComposeCommentView *)sender textDidChange:(NSString *)text {
+    self.mediaItem.temporaryComment = text;
+}
+
+- (void) commentViewWillStartEditing:(ComposeCommentView *)sender {
+    [self.delegate cellWillStartComposingComment:self];
+}
+
+- (void) stopComposingComment {
+    [self.commentView stopComposingComment];
 }
 @end
