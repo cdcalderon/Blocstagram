ed48bddfc61a8c193ec846a99ec8e912bfd426a0
carlos calderon <cdcalderon@gmail.com>
2015-12-07 02:07:40 -0600

    init and position elements for CameraToolbar View

---
+++ Blocstagram/CameraToolbar.m
@@ -0,0 +1,21 @@
+//
+//  CameraToolbar.m
+//  
+//
+//  Created by Carlos Calderon on 12/7/15.
+//
+//
+
+#import "CameraToolbar.h"
+
+@implementation CameraToolbar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
